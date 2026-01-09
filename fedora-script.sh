30b2a1d43fccb063d372d5aca6a8176031ce015d
farhan10ansari <farhan10ansari@gmail.com>
2026-01-09 19:23:56 +0530

    some more changes

--- fedora-script.sh
+++ fedora-script.sh
@@ -4,11 +4,11 @@ sudo dnf upgrade --refresh
 curl -f https://zed.dev/install.sh | sh
 
 # fedora apps
-sudo dnf install git vlc curl adb gnome-tweaks htop
+sudo dnf install git vlc curl adb gnome-tweaks htop gnome-tweaks -y
 
 # flatpak apps
-flatpak install flathub com.mattjakeman.ExtensionManager
-flatpak install flathub io.github.kolunmi.Bazaar
+flatpak install flathub com.mattjakeman.ExtensionManager -y
+flatpak install flathub io.github.kolunmi.Bazaar -y
 flatpak install flathub net.nokyan.Resources -y
 flatpak install flathub org.localsend.localsend_app -y
 flatpak install flathub com.usebottles.bottles -y
@@ -21,6 +21,20 @@ flatpak install flathub com.vixalien.sticky -y
 sudo dnf install @virtualization
 sudo systemctl enable --now libvirtd
 sudo usermod -aG kvm,libvirt $(whoami)
+
+#ddcutil for monitor control
+sudo dnf install ddcutil
+#copyous
+sudo dnf install libgda libgda-sqlite
+
+
+#vscode setup
+sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc &&
+echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\nautorefresh=1\ntype=rpm-md\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" | sudo tee /etc/yum.repos.d/vscode.repo > /dev/null
+
+dnf check-update && sudo dnf install code # or code-insiders
+
+
 
 
 # some settings
